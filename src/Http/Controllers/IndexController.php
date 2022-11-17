6e72f3189d68bbe74d7cd34d07d045692178c260
FishSev <ilya.sevrugin@gmail.com>
2022-11-18 03:10:56 +0600

    добавил прорку статуса бота через админку

--- src/Http/Controllers/IndexController.php
+++ src/Http/Controllers/IndexController.php
@@ -5,13 +5,41 @@ namespace App\Http\Controllers;
 use Slim\Http\Response;
 use Slim\Http\ServerRequest;
 use Slim\Views\Twig;
+use GuzzleHttp\Client;
 
-class IndexController 
+class IndexController
 {
-    public function home(ServerRequest $request,Response $response) 
+    private $client;
+
+    function initializeClient(): void
+    {
+        $client = new Client([
+            'base_uri' => 'http://localhost',
+            'timeout' => 2.0,
+            'verify' => false,
+        ]);
+        $this->client = $client;
+    }
+
+    function home(ServerRequest $request, Response $response)
     {
         $view = Twig::fromRequest($request);
+        $this->initializeClient();
 
-        return $view->render($response, 'home.twig', ['name' => 'guest']);
+        try {
+            $serviceResponse = $this->client->request('GET', '/health', ['proxy' => 'localhost:8888']);
+            $result = json_decode($serviceResponse->getBody()->getContents(), associative: true);
+            $status = $result["status"];
+
+            if ($status == "ok") {
+                return $view->render($response, 'home.twig', ['name' => 'guest', 'status' => $status]);
+            } else {
+                $errMessage = "статус сервиса не активен";
+            };
+        } catch (\Exception $error) {
+            $errMessage = $error->getMessage();
+        }
+
+        return $view->render($response, 'connectError.twig', ['error' => $errMessage]);
     }
 }
