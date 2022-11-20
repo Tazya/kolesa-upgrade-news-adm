8dba8ca69633f48f03a1442daf123ad34636410e
FishSev <ilya.sevrugin@gmail.com>
2022-11-21 01:04:13 +0600

    настроил отправку сообщений в сервис бота #5

--- src/Http/Controllers/MessageController.php
+++ src/Http/Controllers/MessageController.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace App\Http\Controllers;
 
@@ -7,10 +7,11 @@ use Slim\Http\Response;
 use SLim\Views\Twig;
 use App\Model\Validators\MessageValidator;
 use App\Repository\MessageRepository;
+use GuzzleHttp\Client;
 
-class MessageController 
+class MessageController
 {
-    public function new(ServerRequest $request,Response $response) 
+    public function new(ServerRequest $request, Response $response)
     {
         $view = Twig::fromRequest($request);
 
@@ -32,9 +33,27 @@ class MessageController
                 'errors' => $errors,
             ]);
         }
+
+        $data = [
+            'proxy' => 'localhost:8888',
+            'body' => json_encode($messageData),
+        ];
+        $client = new Client();
+        try {
+            $serviceResponse = $client->request('POST', 'http://localhost/messages/sendToAll', $data);
+            $result = json_decode($serviceResponse->getBody()->getContents(), associative: true);
+            
+            if ($result["status"] != "ok"){
+                throw new \Exception($result["error"]);
+            }
+
+        } catch (\Exception $exc) {
+            return $view->render($response, 'sendError.twig', ['error' => $exc->getMessage()]);
+        };
+
         $repo = new MessageRepository();
         $repo->create($messageData);
-        return $view->render($response,'Message/index.twig', ['messages'=> [$messageData]]);
+        return $view->render($response, 'Message/index.twig', ['messages' => [$messageData]]);
     }
 
     public function allMessages(ServerRequest $request, Response $response)
